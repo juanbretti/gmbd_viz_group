2889a0207f05537383a652a0a1ea745888b15715
Juan Pedro Bretti Mandarano <juan+github@bretti.com.ar>
2020-12-08 17:57:43 +0100

    Missing conclusions

--- shiny/app.R
+++ shiny/app.R
@@ -1,6 +1,5 @@
 # Libraries
 library(shiny)
-
 library(tidyverse)
 library(skimr)
 library(ggpmisc)
@@ -10,7 +9,6 @@ library(scales)
 # library(PerformanceAnalytics)
 # Maps
 library(maps)
-
 # https://stackoverflow.com/questions/30790036/error-istruegpclibpermitstatus-is-not-true
 library(rgdal)
 library(maptools)
@@ -30,10 +28,10 @@ text_skim <- skim(df)
 #     select(-tx_date_proc)
 # chart_correlation1 <- chart.Correlation(df_mod, histogram=TRUE, pch=19)
 
-df_mod_class_top <- function(df, class, top) {
+df_mod_class_top <- function(df, class, group, top) {
     df_mod <- df %>% 
         rename(`class`= !!class,
-               `group`= !!(if(class=='category') 'customer_country' else 'category'))
+               `group`= !!group)
     
     df_top_class <- df_mod %>% 
         group_by(`class`) %>% 
@@ -56,8 +54,7 @@ df_mod_class_top <- function(df, class, top) {
     return(df_grouped)
 }
 
-# Distribution plot of the amount
-plot_distribution1 <- function(df, class, top) {
+plot_distribution1 <- function(df, class) {
     mu <- df %>% 
         group_by(top_class) %>% 
         summarise(grp.mean=mean(amount))
@@ -65,19 +62,59 @@ plot_distribution1 <- function(df, class, top) {
     plot_distribution1 <- ggplot(df, aes(x=amount, color=top_class, fill=top_class)) +
         geom_density(alpha=0.1)+
         geom_vline(data=mu, aes(xintercept=grp.mean, color=top_class), linetype="dashed")+
-        labs(title="Amount",x="Amount in log scale", y = "Density")+
-        theme_classic() +
-        scale_x_log10()
-
+        labs(title="Amount", 
+             subtitle=paste('Amount distribution per', class), 
+             x="Amount in log scale", 
+             y = "Density", 
+             fill=class, color=class)+
+        scale_x_log10() +
+        scale_fill_viridis_d(name=class) +
+        theme(axis.text.y=element_blank())
+    
     plot_boxplot1 <- ggplot(df, aes(x=amount, y=top_class, fill=top_class)) +
         geom_boxplot(alpha=0.1) +
         scale_x_log10() +
-        theme(legend.position = "none")
+        theme(legend.position = "none") +
+        scale_fill_viridis_d() +
+        labs(title="Box-plot of the amount", 
+             subtitle=paste('Amount distribution per', class, 'using a box-plot'), 
+             x="Amount in log scale", 
+             y = class, 
+             fill=class, color=class)
     
     return(list(plot_distribution1, plot_boxplot1))
 }
 
-plot_distribution_country_category <- function(df, class, top) {
+plot_distribution2 <- function(df, class) {
+    df_sort <- df %>% 
+        group_by(top_class) %>% 
+        summarise(n_=n(),
+                  sum_=sum(amount),
+                  mean_=mean(amount)) %>% 
+        arrange((sum_)) %>% 
+        .[['top_class']]
+    
+    df_grouped <- df %>% 
+        group_by(top_class, hour) %>% 
+        summarise(n_=n(),
+                  sum_=sum(amount),
+                  mean_=mean(amount)) %>% 
+        mutate(top_class = factor(top_class, df_sort))
+    
+    gg <- ggplot(df_grouped, aes(y=top_class, x=sum_, fill=top_class)) +
+        geom_density_ridges(alpha=0.1) +
+        labs(title = paste(class, "spendings"),
+             subtitle = "Aggregated by hours",
+             y = class,
+             x = "Total spending") +
+        theme(legend.position = "none") +
+        scale_fill_viridis_d()
+    
+    return(gg)
+}
+
+
+plot_distribution_country_category <- function(df, class, group) {
     mu <- df %>% 
         group_by(top_class) %>% 
         summarise(grp.mean=mean(amount))
@@ -86,10 +123,14 @@ plot_distribution_country_category <- function(df, class, top) {
         facet_wrap(vars(top_group)) +
         geom_density(alpha=0.1)+
         geom_vline(data=mu, aes(xintercept=grp.mean, color=top_class), linetype="dashed")+
-        labs(title="Amount",x="Amount", y = "Density") +
+        labs(title = paste(class, "spendings"),
+             subtitle = paste("One plot per", group),
+             y = 'Density',
+             x = "Amount per operation") +
         coord_cartesian(xlim = c(0, 200), ylim = c(0, 0.02)) +
-        theme(legend.position = "none")
-
+        theme(legend.position = "none",
+              axis.text.y=element_blank())
+    
     return(gg)
 }
 
@@ -125,14 +166,14 @@ map_location <- function(df, class) {
         theme_void() +
         theme(legend.position = "none")
     
-    if (class=='category') {
+    if (class=='Category') {
         gg <- gg + facet_wrap(vars(category))
     }
     
     return(gg)
 }
 
-plot_distribution_hourofday <- function(df, class, top) {
+plot_distribution_hourofday <- function(df, class) {
     df_grouped <- df %>% 
         mutate(day_hour_=floor_date(tx_date_proc, "hour"),
                hour_=make_datetime(
@@ -143,20 +184,43 @@ plot_distribution_hourofday <- function(df, class, top) {
     gg <- ggplot(df_grouped, aes(x=hour, color=weekday, fill=weekday)) +
         facet_wrap(vars(top_class)) +
         geom_density(alpha=0.1) +
-        theme(axis.text.x=element_text(angle=90, hjust=1))
-        # coord_cartesian(ylim = c(0, 0.00005))
+        theme(axis.text.x=element_text(angle=90, hjust=1),
+              axis.text.y=element_blank()) +
+        labs(title = paste(class, "spendings"),
+             subtitle = paste("One plot per", class),
+             y = 'Density',
+             x = "Hour of the day")
     
     return(gg)
 }
 
-ui <- navbarPage(title = "CitiBank",
+plot_heatmap_hourofday <- function(df, class) {
+    df_grouped <- df %>% 
+        group_by(top_class, hour) %>% 
+        summarise(n_=n(),
+                  sum_=sum(amount),
+                  mean_=mean(amount))
+    
+    gg <- ggplot(df_grouped, aes(x=hour, y=top_class, fill=sum_)) +
+        geom_tile(alpha=0.8) +
+        theme(axis.text.x=element_text(angle=90, hjust=1)) +
+        scale_fill_viridis_c(name = "Spenditure") +
+        labs(title = "Spendings",
+             subtitle = "Total spenditure",
+             y = class,
+             x = "Hour of the day")
+    
+    return(gg)
+}
+
+ui <- navbarPage(title = "Citibank",
                  tabPanel("Top rules",
                           sidebarLayout(
                               sidebarPanel(
-                                  div(img(src="logo.png",height=194,width=300), style="text-align: center;"),
+                                  div(img(src="logo.png",height=194/2,width=300/2), style="text-align: center;"),
                                   br(),
-                                  sliderInput('top', 'Top rules:', min = 1, max = 50, value = 10),
-                                  selectInput('class', 'Class', choices=c('customer_country', 'category'), selected = 'customer_country'),
+                                  selectInput('class', 'Class', choices=c('Country', 'Category'), selected = 'Country'),
+                                  sliderInput('top', 'Top', min = 1, max = 50, value = 5),
                                   br(),
                                   span("Select the top rules based on the proposed sorting criteria. It's recommended the 'category'.")
                               ),
@@ -175,36 +239,31 @@ ui <- navbarPage(title = "CitiBank",
                                                        br(),
                                                        plotOutput('plot_distribution1_2'),
                                                        br(),
+                                                       plotOutput('plot_distribution2'),
+                                                       br(),
                                                        plotOutput('plot_distribution_country_category')
+                                              ),
+                                              tabPanel("Hour of the day", 
+                                                       span("Click the objects to display the association rule description."),
+                                                       br(),
+                                                       plotOutput('plot_distribution_hourofday'),
+                                                       br(),
+                                                       plotOutput('plot_heatmap_hourofday')
                                               )
                                   )
-                              )
-                          )
-                 ),
-                 tabPanel("Hour of the day",
-                          sidebarLayout(
-                              sidebarPanel(
-                                  div(img(src="logo.png",height=194,width=300), style="text-align: center;"),
-                                  br(),
-                                  span("Distribution considering the hour of the day."),
-                              ),
-                              mainPanel(
-                                    span("Click the objects to display the association rule description."),
-                                    br(),
-                                    plotOutput('plot_distribution_hourofday')
                               )
                           )
                  ),
                  tabPanel("Descriptive study",
                           sidebarLayout(
                               sidebarPanel(
-                                  div(img(src="logo.png",height=194,width=300), style="text-align: center;"),
+                                  div(img(src="logo.png",height=194/2,width=300/2), style="text-align: center;"),
                                   br(),
                                   span("General descriptive information of the transactions and rules")
                               ),
                               mainPanel(
-                                   h3("Datase summary"),
-                                   verbatimTextOutput('skim')
+                                  h3("Datase summary"),
+                                  verbatimTextOutput('skim')
                               )
                           )
                  )
@@ -217,19 +276,29 @@ server <- function(input, output) {
     
     # Distribution
     observeEvent(c(input$class, input$top), ignoreNULL = FALSE, ignoreInit = FALSE, {
-        df_mod <- df_mod_class_top(df, input$class, input$top)
-        
-        plot_temp <- plot_distribution1(df_mod, input$class, input$top)
-        output$plot_distribution1_1 <- renderPlot(plot_temp[1])  
+        # Selection
+        class <- if(input$class=='Category') 'category' else 'customer_country'
+        group <- if(input$class=='Category') 'customer_country' else 'category'
+        group2 <- if(input$class=='Category') 'Country' else 'Category'
+        # Dataset
+        print(class)
+        df_mod <- df_mod_class_top(df, class, group, input$top)
+
+        plot_temp <- plot_distribution1(df_mod, input$class)
+        output$plot_distribution1_1 <- renderPlot(plot_temp[1])
         output$plot_distribution1_2 <- renderPlot(plot_temp[2])
-        
-        output$plot_distribution_country_category <- renderPlot(plot_distribution_country_category(df_mod, input$class, input$top))
+
+        output$plot_distribution2 <- renderPlot(plot_distribution2(df_mod, input$class))
+
+        output$plot_distribution_country_category <- renderPlot(plot_distribution_country_category(df_mod, input$class, group2))
 
         output$map_location <- renderPlot(map_location(df, input$class))
-        
-        output$plot_distribution_hourofday <- renderPlot(plot_distribution_hourofday(df_mod, input$class, input$top))
-    })
 
+        output$plot_distribution_hourofday <- renderPlot(plot_distribution_hourofday(df_mod, input$class))
+
+        output$plot_heatmap_hourofday <- renderPlot(plot_heatmap_hourofday(df_mod, input$class))
+    })
+    
 }
 
 shinyApp(ui = ui, server = server)
